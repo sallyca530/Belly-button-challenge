3087c73517cb758d230b228fe5c0404faa184759
sallyca530 <sally.ca530@gmail.com>
2024-01-19 18:01:50 -0800

    demogr info and readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Belly-button-challenge
+
+#### References
+* Used all Module 14 activities to write code
+* object.entries() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
--- static/js/app.js
+++ static/js/app.js
@@ -5,3 +5,26 @@ const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 // confirm retrieval of data
 const dataPromise = d3.json(url);
     console.log("Data Promise: ", dataPromise);
+
+
+// //////////////////////////////////////////////////////////////////
+// Pull the Json data 
+// Sample data function for the "demographics info"
+function demoInfo(id) {
+    d3.json(url).then(function (data) {
+        // define variables 
+        let sampleData = data;
+        // get metadata
+        let metadata = sampleData.metadata;
+        // filter sample IDs
+        let ID = metadata.filter(sample =>
+            sample.id.toString() === id)[0];
+        // display/panel info
+        let demoPanel = d3.select('#sample-metadata');
+        demoPanel.html('');
+        // returns the each test subjects ID demographic info
+        Object.entries(ID).forEach(([key, value]) => {
+            demoPanel.append('h6').text(`${key}: ${value}`);
+        })
+    })
+};
